b4642ec93e655c2ded1c4e41edf15a723e3c18b5
radityagumay <gumay.raditya@yahoo.com>
2017-01-08 21:55:18 +0700

    first commit

---
+++ com/radityalabs/main.py
@@ -0,0 +1,16 @@
+import csv
+## http://machinelearningmastery.com/naive-bayes-classifier-scratch-python/
+
+
+def loadData(filename):
+    lines = csv.reader(open(filename, "rb"))
+    dataset = list(lines)
+    for i in range(len(dataset)):
+        dataset[i] = [float(x) for x in dataset[i]]
+    return dataset
+
+
+filename = "data.csv"
+dataset = loadData(filename)
+print ('loaded data file {0} with {1} rows').format(filename, len(dataset))
+g
